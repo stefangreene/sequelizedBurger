48e7d5bde01900a1be481649d4552fc86a805e60
stefangreene <stefangreene@comcast.net>
2019-03-31 02:10:41 -0400

    changes

---
+++ app/db/schema.sql
@@ -0,0 +1,16 @@
+
+/*schema//SMG BURGER EATER SCHEMA..................
+
+DROP DATABASE IF EXISTS hamburgers_db;
+CREATE DATABASE hamburgers_db;
+USE hamburgers_db;
+
+
+CREATE TABLE burgers (
+id INT (10) AUTO_INCREMENT NOT NULL,
+burger VARCHAR (40) NOT NULL,
+eaten BOOLEAN,
+PRIMARY KEY (id)
+);
+
+*/
---
+++ app/db/seeds.sql
@@ -0,0 +1,11 @@
+
+/* SEEDS......... HERE is the burgers..... BRING ON THE MEAT.....
+
+INSERT INTO burgers (burger, eaten) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger, eaten) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger, eaten) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger, eaten) VALUES ('Tofu Gluten Free Burger', false);
+INSERT INTO burgers (burger, eaten) VALUES ('Cauliflower Burger', true);
+INSERT INTO burgers (burger, eaten) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger, eaten) VALUES ('Steak and Cheese Burger', true);
+INSERT INTO burgers (burger, eaten) VALUES ('Lettuce wrapped Kobe Beef', false);

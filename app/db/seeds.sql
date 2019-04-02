a6a3064b9c7c459f0ec2083f506f3023b26afeb3
stefangreene <stefangreene@comcast.net>
2019-04-02 12:12:05 -0400

    changes

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -1,15 +1,13 @@
 
 /* SEEDS......... HERE is the burgers..... BRING ON THE MEAT.....
 
-INSERT INTO burgers (burger, eaten) VALUES ('Hamburger', false);
-INSERT INTO burgers (burger, eaten) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger, eaten) VALUES ('Bacon Cheeseburger', false);
-INSERT INTO burgers (burger, eaten) VALUES ('Tofu Gluten Free Burger', false);
-INSERT INTO burgers (burger, eaten) VALUES ('Cauliflower Burger', true);
-INSERT INTO burgers (burger, eaten) VALUES ('Mushroom Burger', true);
-INSERT INTO burgers (burger, eaten) VALUES ('Steak and Cheese Burger', true);
-INSERT INTO burgers (burger, eaten) VALUES ('Lettuce wrapped Kobe Beef', false);
-INSERT INTO burgers (burger, eaten) VALUES ('Veggie Burger', true);
-INSERT INTO burgers (burger, eaten) VALUES ('Broccoli Burger', true);
-INSERT INTO burgers (burger, eaten) VALUES ('Double Stacked Heart Attack', true);
-INSERT INTO burgers (burger, eaten) VALUES ('Southwest Cheese and Chili', false);
+INSERT INTO burgers (name, eaten) VALUES ('Hamburger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Tofu Gluten Free Burger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Steak and Cheese Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Broccoli Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Double Stacked Heart Attack', true);
+INSERT INTO burgers (name, eaten) VALUES ('Southwest Cheese and Chili', false);
